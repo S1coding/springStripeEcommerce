79faeb449177862d16c8bbb18cef78afa31e81e7
s1coding <sajjansharmab@gmail.com>
2025-04-26 12:54:41 +0100

    modify commit: changed application class name

--- src/test/java/ecommerceService/springStripeEcommerceService/SpringStripeEcommerceServiceApplicationTests.java
+++ src/test/java/ecommerceService/springStripeEcommerceService/SpringStripeEcommerceServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package adidasCopy.adidasCopyBackEnd;
+package ecommerceService.springStripeEcommerceService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
